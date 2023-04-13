358bdd426552ff2c270ef3a103fd3127a6736798
bimanshu <130674393+bimanshu@users.noreply.github.com>
2023-04-13 23:29:05 +0530

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,69 @@
+/* let word= prompt("write a number").split("");
+
+for (var i = 0; i < word.length - 1; i+=3) 
+{
+     word[i] = "x";
+}
+let a= word.join("");
+console.log(a);
+
+................................................................................................................
+
+for (i=0; i<7; i++){
+  console.log(fr[i]);
+}*/
+
+/*const ab= " is a baka"
+console.log(ab.endsWith('baka'));*/
+
+/*const ab="please give rs 1000"
+console.log(ab.length);
+console.log(ab.slice(15,20));*/
+
+
+
+/*const ar=[1,45,34,67]
+for (i=0; i<ar.length;i++){
+  console.log(ar[i])
+}*/
+
+/*const ar=[1,45,34,67]
+let r= ar.pop();
+console.log(ar);*/
+
+/*
+let num =prompt ("enter your number");
+if (num%2==0 && num%3==0){
+console.log ("your number is divisible by 2 and 3 ")
+}
+else if (num%2==0)
+{
+console.log ("your number is divisible by 2");
+}
+else if (num%3==0){
+console.log ("your number is divisible by 3");
+}
+else {
+console.log ("your number is not divisible by either 2 or 3");
+}
+
+*/
+
+/*
+let age = prompt("What is your age?")
+switch (age) {
+  case '12':
+    console.log("Your age is 12")
+    break
+  case '13':
+    console.log("Your age is 13")
+    break
+  case '14':
+    console.log("Your age is 14")
+    break
+  case '15':
+    console.log("Your age is 15")
+    break
+  default:
+    console.log("Your age is not special")
+    */
